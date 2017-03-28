da4894d1f585531d4f6977c2b791a7f79afcfa48
Daniil Kolesnichenko <d.s.kolesnichenko@yandex.ru>
2017-03-28 14:08:28 +0000

    Add date/time to the log message

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,45 @@
 extern crate log;
 extern crate colored;
+extern crate chrono;
 
-use log::{LogLevel, LogMetadata, LogRecord};
+use log::{LogLevel, LogLevelFilter, LogMetadata, LogRecord, SetLoggerError};
 use colored::*;
 
 struct ColoredLogger;
 
 impl log::Log for ColoredLogger {
-    fn enabled(&self, _: &LogMetadata) -> bool {
-        true
+    fn enabled(&self, metadata: &LogMetadata) -> bool {
+        metadata.level() <= LogLevel::Info
     }
 
     fn log(&self, record: &LogRecord) {
         if self.enabled(record.metadata()) {
+            let time_string = chrono::Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+
             match record.level() {
                 LogLevel::Error => {
-                    println!("[{}] {}", "error".red(), record.args());
+                    println!("[{}] {}", time_string.red(), record.args());
                 }
                 LogLevel::Warn => {
-                    println!("[{}] {}", "warn".yellow(), record.args());
+                    println!("[{}] {}", time_string.yellow(), record.args());
                 }
                 LogLevel::Info => {
-                    println!("[{}] {}", "info".blue(), record.args());
+                    println!("[{}] {}", time_string.blue(), record.args());
                 }
                 LogLevel::Debug => {
-                    println!("[{}] {}", "debug".white(), record.args());
+                    println!("[{}] {}", time_string.white(), record.args());
                 }
                 LogLevel::Trace => {
-                    println!("[{}] {}", "trace".white(), record.args());
+                    println!("[{}] {}", time_string.white(), record.args());
                 }
             }
         }
     }
+}
+
+pub fn init() -> Result<(), SetLoggerError> {
+    log::set_logger(|max_log_level| {
+        max_log_level.set(LogLevelFilter::Info);
+        Box::new(ColoredLogger)
+    })
 }
